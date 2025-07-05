9f467fdc6c80d37f84389bf8498d5201b339f394
valevurro <vale.v.00@outlook.it>
2025-07-05 10:37:30 +0200

    Add variable assignments and string operations in script.js

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,8 +2,8 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
-let Name;
-Name = 'Valentina'; 
+
+const Name = 'Valentina'; 
 console.log(Name); 
 
 
